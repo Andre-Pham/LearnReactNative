583fd74f0c02dda7b2d90b963e0caca66108373e
Andre Pham <andrepham33@gmail.com>
2023-03-31 14:33:21 +1100

    StateDemo init

---
+++ StateDemo/StateDemo/StateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateDemoApp.swift
+//  StateDemo
+//
+//  Created by Andre Pham on 31/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct StateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
